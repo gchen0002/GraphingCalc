bed83402e78928467066d1bda21a52b736d188f5
Guanying Chen <xnohohsd1@gmail.com>
2025-05-21 23:57:36 -0700

    basic grid made

--- includes/animate/graph.cpp
+++ includes/animate/graph.cpp
@@ -9,6 +9,27 @@ void Graph::update(int command){
 }
 void Graph::draw(sf::RenderTarget& target) {
     set_info();
+
+    // 2 points for each axis
+    sf::VertexArray grid_axis(sf::PrimitiveType::Lines, 4);
+
+    // x-axis
+    float x_axis_y_position = SCREEN_HEIGHT / 2.0f;
+    grid_axis[0].position = sf::Vector2f(0, x_axis_y_position);
+    grid_axis[0].color = sf::Color::White;
+    grid_axis[1].position = sf::Vector2f(WORK_PANEL, x_axis_y_position);
+    grid_axis[1].color = sf::Color::White;
+
+    // y-axis
+    float y_axis_x_position = WORK_PANEL / 2.0f;
+    grid_axis[2].position = sf::Vector2f(y_axis_x_position, 0);
+    grid_axis[2].color = sf::Color::White;
+    grid_axis[3].position = sf::Vector2f(y_axis_x_position, SCREEN_HEIGHT);
+    grid_axis[3].color = sf::Color::White;
+
+    target.draw(grid_axis);
+
+    // Draw Points from Plot
     sf::CircleShape pointShape(3.0f); // Small circle for each point
     pointShape.setFillColor(sf::Color::Yellow);
 
@@ -19,5 +40,5 @@ void Graph::draw(sf::RenderTarget& target) {
 } 
 
 void Graph::set_info(){
-    _points = Plot();
+    _points = Plot.operator()(); 
 }
